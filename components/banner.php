881432c7b669976696feb663d55b4f119a15dccd
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-12-01 19:57:38 +0000

    Added HP banner component

---
+++ components/banner.php
@@ -0,0 +1,68 @@
+<section class="banner">
+    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="ratio: 7:3; animation: fade; autoplay: true; autoplay-interval: 5000">
+
+        <ul class="uk-slideshow-items">
+            <li>
+                <div class="uk-position-cover" uk-slideshow-parallax="scale: 1.2,1.2,1">
+                    <img src="<?php echo get_template_directory_uri() . "/assets/images/female-nurse-close.jpg" ?>" alt="female nurse" uk-cover>
+                </div>
+                
+                <div class="uk-position-cover" uk-slideshow-parallax="opacity: 0,0,0.2; backgroundColor: #000,#000"></div>
+
+                <div class="uk-overlay uk-overlay-default uk-position-center uk-position-small uk-text-center">
+                    <small>Access Medics</small>
+                    <h3 class="uk-margin-remove">Quality Care at Home Services.</h3>
+                    <br>
+                    <a href="/home-care" class="blue-btn">Learn More</a>
+                </div>
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri() . "/assets/images/male-nurse.jpg" ?>" alt="male nurse" uk-cover>
+                <div class="uk-overlay uk-overlay-default uk-position-center uk-position-small uk-text-center">
+                    <small>We are recruiting</small>
+                    <h3 class="uk-margin-remove">Nurses, Care Assistants & Support Workers.</h3>
+                    <br>
+                    <a href="/apply-now" class="blue-btn">Apply Now</a>
+                </div>
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri() . "/assets/images/hospital-cleaner.jpg" ?>" alt="hospital cleaner" uk-cover>
+                <div class="uk-overlay uk-overlay-default uk-position-center uk-position-small uk-text-center">
+                    <small>Access Medics</small>
+                    <h3 class="uk-margin-remove">Home Care Your Family With Love.</h3>
+                    <br>
+                    <a href="/home-care" class="blue-btn">Learn More</a>
+                </div>
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri() . "/assets/images/female-nurse.jpg" ?>" alt="female nurse" uk-cover>
+                <div class="uk-overlay uk-overlay-default uk-position-center uk-position-small uk-text-center">
+                    <small>Trained & Vetted</small>
+                    <h3 class="uk-margin-remove">Our Staff are Carefully Selected.</h3>
+                    <br>
+                    <a href="/home-care" class="blue-btn">Learn More</a>
+                </div>
+            </li>
+        </ul>
+
+        <div class="uk-light">
+            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+        </div>
+
+    </div>
+</section>
+
+<section class="uk-container uk-text-center cta-block">
+    <div class="uk-child-width-1-3@m" uk-grid>
+        <div>
+            <h3><i class="fa-solid fa-clipboard-user"></i> Are you looking for staff?</h3>
+        </div>
+        <div>
+            <h3><i class="fa-solid fa-user-nurse"></i> Are you looking for work?</h3>
+        </div>
+        <div>
+            <h3><i class="fa-solid fa-hospital-user"></i> Are you looking for care?</h3>
+        </div>
+    </div>
+</section>
--- front-page.php
+++ front-page.php
@@ -1,2 +1,5 @@
 <?php get_header(); ?>
+
+<?php get_template_part('/components/banner'); ?>
+
 <?php get_footer(); ?>
