2efe04f413f74d62fa8ff4dbfbb48476e8bf52c8
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-12-10 10:27:05 +0000

    added screenshot.png

---
+++ templates/forms-pages.php
@@ -0,0 +1,22 @@
+<?php get_header(); 
+/** Template Name: Forms Pages */
+?>
+
+<header class="page-header">
+    <div class="uk-container">
+        <section class="page-header-grid">
+            <div class="page-header-left"></div>
+            <div class="page-header-right">
+                <h1><?php the_title(); ?></h1>
+            </div>
+        </section>
+    </div>
+</header>
+
+<section class="page-content">
+    <div class="uk-container">
+        <?php the_content(); ?>
+    </div>
+</section>
+
+<?php get_footer(); ?>
